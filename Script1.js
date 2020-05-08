a947802e79d1ec0a9523611f107115362e2ed1a6
Maria Karlsson <mariahkarlsson_@hotmail.com>
2020-05-08 08:42:44 +0200

    onclick event expanded

--- Script1.js
+++ Script1.js
@@ -2,17 +2,22 @@
 console.log("Script is running")
 
 // when called this function will add a drop down menu
-function dropDown(){
+function dropDown() {
     console.log("adding drop down list")
 
-    var dropDownDiv = document.createElement("div"); // creates a div element to put drop down list into
+    var dropDownDiv = document.createElement("div"); // creates a div element to put everything into
+    dropDownDiv.className = "dropdownDiv"; // assigning classname to access element in css file
+
     var button = document.createElement("button"); // drop down list button
-    button.innerHTML= "Choose country";
-    
+    button.className = "dropbtn";
+    button.innerHTML = "Choose country";
+
     var fullList = document.createElement("li"); // list to place al items in
+    fullList.id = "dropdown-content"; // assigning id to get this element later on
+    fullList.className = "dropdown-content"; // assigning classname for css file, and also window.onclick event
 
     // will be replaced by mockAPI countries
-    var item = [ 
+    var item = [
         ["country 1"],
         ["country 2"],
         ["country 3"],
@@ -27,12 +32,26 @@ function dropDown(){
         fullList.appendChild(listItem); // adding paragraph to list element
     }
 
-    button.addEventListener ("click",
-    function(){
-        
-    })
+    button.addEventListener("click",
+        function dropTheList() {
+            document.getElementById("dropdown-content").classList.toggle("show");
+        })
+    
+    // event to close dropdown list when clicking anywhere else 
+    window.onclick = function (event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
 
-    //  ading al elements to document body
+    //  adding all elements to document body
     button.appendChild(fullList);
     dropDownDiv.appendChild(button);
     document.body.appendChild(dropDownDiv);
@@ -40,6 +59,3 @@ function dropDown(){
 }
 
 dropDown();
-
-
-
